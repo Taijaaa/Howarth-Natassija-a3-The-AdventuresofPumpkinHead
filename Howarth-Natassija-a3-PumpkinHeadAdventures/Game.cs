cf4a717fb71713fce0c7dfdebfcd0a062dd82c8b
Taijaaa <000903701@mohawkcollege.ca>
2025-03-11 18:34:47 -0400

    lots of plotting of platforms

--- Howarth-Natassija-a3-PumpkinHeadAdventures/Game.cs
+++ Howarth-Natassija-a3-PumpkinHeadAdventures/Game.cs
@@ -20,6 +20,30 @@ namespace MohawkGame2D
         Texture2D Platform2 = 
             Graphics.LoadTexture("../../../../assets/graphics/Platform2.png");
 
+        Texture2D Platform3 =
+            Graphics.LoadTexture("../../../../assets/graphics/Platform3.png");
+
+        Texture2D Platform4 =
+            Graphics.LoadTexture("../../../../assets/graphics/Platform4.png");
+
+        Texture2D Platform5 =
+            Graphics.LoadTexture("../../../../assets/graphics/Platform4.png");
+
+        Texture2D Platform6 =
+            Graphics.LoadTexture("../../../../assets/graphics/Platform3.png");
+
+        Texture2D Platform7 =
+            Graphics.LoadTexture("../../../../assets/graphics/Platform4.png");
+
+        Texture2D Platform8 =
+            Graphics.LoadTexture("../../../../assets/graphics/Platform2.png");
+
+        Texture2D Bridge1 =
+            Graphics.LoadTexture("../../../../assets/graphics/Bridge1.png");
+
+        Texture2D Bridge2 =
+            Graphics.LoadTexture("../../../../assets/graphics/Bridge2.png");
+
         Texture2D PumpkinHead =
             Graphics.LoadTexture("../../../../assets/graphics/PumpkinHead.png");
 
@@ -52,8 +76,39 @@ namespace MohawkGame2D
         {
             Window.ClearBackground(Color.OffWhite);
 
-            Graphics.Draw(Platform1, 0, 0);
-            Graphics.Draw(Platform2, -292, 241);
+            // bridge railing
+
+            Graphics.Draw(Bridge1, 260, 470);
+
+            // bridge plank 
+
+            Graphics.Draw(Bridge2, 260, 486);
+
+            // left side platforms
+
+            Graphics.Draw(Platform4, 100, 460);
+
+            Graphics.Draw(Platform1, 0, 420);
+
+            Graphics.Draw(Platform2, 60, 500);
+
+            Graphics.Draw(Platform3, 55, 570);
+
+            // right side platforms
+
+            Graphics.Draw(Platform8, 570, 410);
+
+            Graphics.Draw(Platform5, 453, 445);
+
+            Graphics.Draw(Platform7, 520, 500);
+
+            Graphics.Draw(Platform6, 400, 570);
+
+            
+
+
+
+
             Graphics.Draw(PumpkinHead, 250, 265);
             Graphics.Draw(Bats, 100, 70);
             Graphics.Draw(Candy2, 100, 65);
